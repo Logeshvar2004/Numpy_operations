fce4603ccc2a8d1a35baad46e1f486f2d72fe7c9
Logeshvar2004 <102981016+Logeshvar2004@users.noreply.github.com>
2022-10-14 23:40:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ def calculate(list):
   ls= np.array(list)
   ls=ls.reshape(3,3)
   
+  #mean of rows & colums:
   mnr=[ls[0,:].mean(),ls[1,:].mean(),ls[2,:].mean()]
   mnc=[ls[:,0].mean(),ls[:,1].mean(),ls[:,2].mean()]
-
+  
+  #Variance of rows and columns:
   varr=[ls[0,:].var(),ls[1,:].var(),ls[2,:].var()]
   varc=[ls[:,0].var(),ls[:,1].var(),ls[:,2].var()]
   
+  #Standard deviation of rows and columns:
   stdr=[ls[0,:].std(),ls[1,:].std(),ls[2,:].std()]
   stdc=[ls[:,0].std(),ls[:,1].std(),ls[:,2].std()]
 
-  
+  #Finding Max value:
   maxr=[ls[0,:].max(),ls[1,:].max(),ls[2,:].max()]
   maxc=[ls[:,0].max(),ls[:,1].max(),ls[:,2].max()]
 
-  
+  #Finding Min value:
   minr=[ls[0,:].min(),ls[1,:].min(),ls[2,:].min()]
   minc=[ls[:,0].min(),ls[:,1].min(),ls[:,2].min()]
-
+  
+  #Finding Sum:
   sumr=[ls[0,:].sum(),ls[1,:].sum(),ls[2,:].sum()]
   sumc=[ls[:,0].sum(),ls[:,1].sum(),ls[:,2].sum()]
   
